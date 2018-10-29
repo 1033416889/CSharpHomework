593c49f12c5a64940c26eb5745732d15450ac019
卜江 黄 <hbj1033416889@outlook.com>
2018-10-29 22:34:14 +0800

    0

--- CSharpHomeworkWeek8/CSharpHomeworkProject1/Form5.Designer.cs
+++ CSharpHomeworkWeek8/CSharpHomeworkProject1/Form5.Designer.cs
@@ -61,7 +61,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form5";
-            this.Text = "Form5";
+            this.Text = "提示";
             this.ResumeLayout(false);
             this.PerformLayout();
 
